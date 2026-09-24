83a79dd445c691137e8f9d82f3d7780fef6dbc4d
Chamogelastos <stavros.ssta@gmail.com>
2026-09-24 17:30:41 +0300

    Removed module-info.java from the paper side, because it was causing issues with placeholderapi

--- sqdlib-paper/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module sqd.lib.sqdlib.paper.main {
-    requires java.net.http;
-    requires net.kyori.adventure.api;
-    requires net.kyori.adventure.key;
-    requires org.bukkit;
-    requires org.jspecify;
-    requires placeholderapi;
-    requires sqd.lib.sqdlib.core.main;
-}
